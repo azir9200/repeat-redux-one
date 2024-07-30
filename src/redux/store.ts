efae68ab4b8a85662c8e4403cc9633ce12d5a259
Azir@9200 <aziruddin83@hotmail.com>
2024-07-30 13:23:26 +0200

    added

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from './features/counterSlice';
-
+import counterReducer from "./features/counterSlice";
 
 export const store = configureStore({
-    reducer: {
-         counter: counterReducer,
-
-    
-    },
+  reducer: {
+    counter: counterReducer,
+  },
+  //   devTools: false,
 });
 
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
